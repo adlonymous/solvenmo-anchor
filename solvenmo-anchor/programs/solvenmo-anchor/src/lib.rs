460072ba0793c848397d3b1c13b1fd6aba8c10df
adlonymous <adlonymous@gmail.com>
2024-10-30 16:25:27 -0400

    added a few anchor functions

--- solvenmo-anchor/programs/solvenmo-anchor/src/lib.rs
+++ solvenmo-anchor/programs/solvenmo-anchor/src/lib.rs
@@ -11,6 +11,56 @@ pub mod solvenmo_anchor {
         cash_account.owner = ctx.accounts.signer.key;
         cash_account.friends = Vec::new();
     }
+
+    pub fn deposit_funds(ctx: Context<DepositFunds>, amount: u64) -> Result<()> {
+        require!(amount > 0, ErrorCode::InvalidAmount);
+
+        let ix = system_instruction::transfer(ctx.accounts.signer.key(), ctx.accounts.cash_account.to_account_info().key, amount);
+
+        invoke(
+            &ix,
+            &[ctx.accounts.signer.clone(), ctx.accounts.cash_account.to_account_info()],
+        )?;
+
+        Ok(())
+    }
+
+    pub fn withdraw_funds(ctx: Context<WithdrawFunds>, amount: u64) -> Result<()> {
+        require!(amount > 0, ErrorCode::InvalidAmount);
+
+        let cash_account = &mut ctx.accounts.cash_account;
+        let wallet = &mut ctx.accounts.signer.to_account_info();
+
+        require!(*cash_account.owner == ctx.accounts.signer.key(), ErrorCode::InvalidSigner);
+
+        **cash_account.try_borrow_mut_lamports()? -= amount;
+        **wallet.try_borrow_mut_lamports()? += amount;
+
+        Ok(())
+    }
+
+    pub fn transfer_funds(
+        ctx: Context<TransferFunds>,
+        _recipient: Pubkey,
+        amount: u64,
+    ) -> Result<()> {
+        require!(amount > 0, ErrorCode::InvalidAmount);
+
+        let from_cash_account = &mut ctx.accounts.from_cash_account.to_account_info();
+        let to_cash_account = &mut ctx.accounts.to_cash_account.to_account_info();
+
+        require!(*cash_account.owner == ctx.accounts.signer.key(), ErrorCode::InvalidSigner);
+
+        **from_cash_account.try_borrow_mut_lamports()? -= amount;
+        **to_cash_account.try_borrow_mut_lamports()? += amount;
+
+        Ok(())
+    }
+    pub fn add_friend(ctx: Context<AddFriend>, pubkey: Pubkey) -> Result<()> {
+        let cash_account = &mut ctx.accounts.cash_account;
+        cash_account.friends.push(pubkey);
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -28,6 +78,64 @@ pub struct InitializeAccount<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct DepositFunds<'info> {
+    #[account(
+        mut,
+        seeds = [b"cash-account". signer.key().as_ref()],
+        bump,
+    )]
+    pub cash_account: Account<'info, CashAccount>,
+    #[account(mut)]
+    pub signer: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+pub struct WithdrawFunds<'info> {
+    #[account(
+        mut,
+        seeds = [b"cash-account", signer.key().as_ref()],
+        bump,
+    )]
+    pub cash_account: Account<'info, CashAccount>,
+    #[account(mut)]
+    pub signer: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
+
+#[derive(Accounts)]
+#[instruction(recipient: Pubkey)]
+pub struct TransferFunds<'info> {
+    #[account(
+        mut,
+        seeds = [b"cash-account", signer.key().as_ref()],
+        bump,
+    )]
+    pub cash_account: Account<'info, CashAccount>,
+    #[account(
+        mut,
+        seeds = [b"cash-account", recipient.as_ref()],
+        bump
+    )]
+    pub to_cash_account: Account<'info, CashAccount>,
+    pub system_program: Program<'info, System>,
+    pub signer: Signer<'info>,
+}
+
+#[derive(Accounts)]
+pub struct AddFriend<'info> {
+    #[account(
+        mut,
+        seeds = [b"cash-account", signer.key().as_ref()],
+        bump,
+    )]
+    pub cash_account: Account<'info, CashAccount>,
+    #[account(mut)]
+    pub signer: AccountInfo<'info>,
+    pub system_program: Program<'info, System>,
+}
+
 #[account]
 #[derive(InitSpace)]
 pub struct CashAccount {
@@ -40,5 +148,6 @@ pub struct CashAccount {
 pub enum ErrorCode {
     #[msg("The provided amount must be greater than 0")]
     InvalidAmount,
-
+    #[msg("Signer does not have access to call this instruction.")]
+    InvalidSigner,
 }
